3312598c2ac741985ecb90f10a944a21e43c4f76
Romulo Rocha <romulorocha@fortalsistemas.com.br>
2019-09-10 09:19:08 -0300

    Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
   function handleAdd() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('@hooks/tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+
+    return () => {};
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@hooks/tech', JSON.stringify(tech));
+  }, [tech]);
+
   return (
     <>
       <button type="button" onClick={handleAdd}>
